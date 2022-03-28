cffb9723751106b01846911a9fab2d5ce474a091
dawabar <elmore.matthew@gmail.com>
2022-03-27 21:47:06 -0400

    pushing what I have, in case I have further issues.  This is after a reboot.

--- src/com/skilldistillery/foodtruck/app/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruck.java
@@ -6,19 +6,58 @@ public class FoodTruck {
 	private String name;
 	private String foodType;
 	private int rating;
-	static int idGenerator = 1001;
-	
-	public FoodTruck(String name, String foodType, int rating) {
-		this.name = name;
-		this.foodType = foodType;
-		this.rating = rating;
-		this.idNumber = idGenerator;
-	}
-
 	
 	public FoodTruck() {}
 	
-	public void incrementIdGenerator () {
-		this.idGenerator = this.idGenerator + 1;
+	public FoodTruck(String name, String foodType, int rating, int idNumber) {
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.idNumber = idNumber;
 	}
+	
+	public int getIdNumber() {
+		return idNumber;
+	}
+	
+	public void setIdNumber(int idNumber) {
+		this.idNumber = idNumber;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getFoodType() {
+		return foodType;
+	}
+	
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	public void greeting() {
+		System.out.println("Hello!  Welcome to the Food Truck Rating Application!");
+		System.out.println();
+		System.out.println("You will be able to add up to five different food trucks,");
+		System.out.println("with their name and the type of food they serve");
+		System.out.println("(tacos, pizza, ribs, burgers, sushi, whatever).");
+		System.out.println();
+		System.out.println("You will be able to give each food truck a rating");
+		System.out.println("(1-5, with 1 being worst and 5 being best).");
+		System.out.println();
+	}
+
 }
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -3,10 +3,12 @@ package com.skilldistillery.foodtruck.app;
 import java.util.Scanner;
 
 public class FoodTruckApp {
-	
 
 	public FoodTruck trucks[];
-	
+	private static int idGenerator = 1001;
+
+//		Create new Scanner for user inputs
+
 	public static void main(String[] args) {
 //		User Story #1
 //		The user is prompted to input the name, food type, and rating for up to five food trucks. 
@@ -17,36 +19,93 @@ public class FoodTruckApp {
 //		The truck id is not input by the user, but 
 //			instead assigned automatically in the FoodTruck constructor 
 //			from a static field that is incremented as each truck is created.
-		
-//		Create new Scanner for user inputs
+
 		Scanner sc = new Scanner(System.in);
 
-//		Send introduction to user
-		UserInteraction io = new UserInteraction();
-		io.greeting();
-//		io.makeChoice();
-		
+		//		Send introduction to user
+		FoodTruck.greeting();
+
+//		Make a choice about whether to add another truck to the array
 		int truckArrayCount = 0;
-		while (truckArrayCount < 5) {
+		boolean keepGoing = true;
+		String userInput = "dummy data";
+		while (truckArrayCount < 5 && keepGoing == true) {
 			System.out.println("Do you want to enter a truck?");
 			System.out.println("Enter 'yes' to add a truck, enter 'quit' when you're done.");
-			String userInput = sc.nextLine();
+			userInput = sc.next();
 			userInput = userInput.trim();
-			if (userInput.charAt(0) == 'q' || userInput.charAt(0) == 'Q') {
+			System.out.println("Input value was " + userInput);
+			if (userInput.trim().toLowerCase().equals("quit")) {
+				keepGoing = false;
+//				TODO remove this line -- System.out.println(userInput);
+				sc.nextLine();
 				break;
-			} 
-			else if (userInput.charAt(0) != 'y' || userInput.charAt(0) != 'Y') {
+			} else if (userInput.trim().toLowerCase().equals("yes")) {
+				System.out.println(userInput);
+				System.out.println("User chose YES");
+				truckArrayCount = getTrucks(truckArrayCount);
+				break;
+			} else if (!userInput.trim().toLowerCase().equals("yes") && !userInput.equals("")) {
+				System.out.println(userInput);
 				System.out.println("Please enter a valid choice.");
 				continue;
-			} 
-			else {
-				trucks[truckArrayCount] = UserInteraction.getTrucks();
-				truckArrayCount++;
 			}
 		}
 
+		boolean keepDisplaying = true;
+		int choice;
+		do {
+			System.out.println("########################################");
+			System.out.println("# 1. List all food trucks              #");
+			System.out.println("# 2. Average rating of all food trucks #");
+			System.out.println("# 3. Highest rated food truck          #");
+			System.out.println("# 4. Quit application                  #");
+			System.out.println("########################################");
+			System.out.println();
+			System.out.println("Choose an option (1-4): ");
+			choice = sc.nextInt();
+			if (choice < 1 || choice > 4) {
+				System.out.println("Please enter a valid choice (1-4)");
+				continue;
+			}
+			else {
+				switch (choice)
+			}
+			while (keepDisplaying = true) {
+		}
+			if (keepDisplaying == false) {
+				System.out.println("Thank you for using this system. Goodbye.");
+				break;
+			}
+			else if (choice < 1 || choice)
+
+		}
 
 	}
 
-}
 
+	// Add a truck to the trucks array
+	public int getTrucks(int count) {
+
+		String nameTemp = "";
+		String foodTemp = "";
+		int ratingTemp = 0;
+		FoodTruck newFT;
+		System.out.println("Enter a truck name: \n");
+		nameTemp = sc.nextLine();
+		System.out.println("What kind of food does it serve: \n");
+		foodTemp = sc.nextLine();
+		System.out.println("Give the food truck a rating (1-5): \n");
+		ratingTemp = sc.nextInt();
+		while (ratingTemp < 1 || ratingTemp > 5) {
+			System.out.println("The rating must be a number between 1 and 5.");
+			System.out.println("Give the food truck a rating (1-5): \n");
+			ratingTemp = sc.nextInt();
+		}
+		newFT = new FoodTruck(nameTemp, foodTemp, ratingTemp, idGenerator);
+		trucks[count] = newFT;
+		idGenerator += 1;
+		return count++;
+	}
+
+}
